e13cac2ed69dcabe4e78ef410579912b2ae2377c
Nathan Houle <ndhoule@gmail.com>
2012-10-18 15:47:05 -0700

    (Finally) write a setup script

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,21 +1,37 @@
 #!/usr/bin/env sh
-
-# This script isn't done, so let's make sure it doesn't get executed
-if [ 1 == 1 ]; then
-    exit 1
-fi
-
 #
 # Run this script at first install.
 #
 
-# Back up and link these files in home dir:
-.oh-my-zsh
-.tmux.conf
-.vim
-.vimrc
-.vimrc.bundles
-.zshrc
+# Enable error checking on script
+set -e
 
-# Update Vim's bundles
+# Set some variables
+WORKINGDIR=$(pwd)
+DOTFILEDIR=$(dirname $(readlink -f $0))
+BACKUPDIR=~/.dotfiles.bak
+DOTFILES=$(ls -a $DOTFILEDIR | grep "^\." | grep -v -e "^..\?$" -e ".git")
+
+
+# Clone down any git submodules
+git submodule update --init
+
+# Back up existing dotfiles
+for file in $DOTFILES; do
+    if [ -e ~/$file ]; then
+        echo "Backing up $file..."
+        mkdir -p $BACKUPDIR
+        mv ~/$file $BACKUPDIR/
+    fi
+done
+
+# Link the dotfiles into place
+for file in $DOTFILES; do
+    ln -s ${DOTFILEDIR}/${file} ~/${file}
+done
+
+# Update Vim's bundles.
+# Couldn't get the pure CLI method to work. Replace this when I figure it out.
 vim +BundleInstall! +BundleClean +qall
+
+echo "Now change and reload your shell for changes to take effect."
