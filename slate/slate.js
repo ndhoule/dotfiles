66d7788e748bc05710abaeaee8cdd58d9c50e9a7
Nathan Houle <nathan@nathanhoule.com>
2013-09-07 11:26:33 -0700

    Add new slate screen orientations

--- slate/slate.js
+++ slate/slate.js
@@ -1,60 +1,80 @@
+/*global S:false*/
+
 slate.configAll({
-  'defaultToCurrentScreen': true,
-  'checkDefaultsOnLoad': true
+  defaultToCurrentScreen: true,
+  checkDefaultsOnLoad: true
 });
 
-var layout = {
-  maximize: S.op('move', {
-    'x' : 'screenOriginX',
-    'y' : 'screenOriginY',
-    'width' : 'screenSizeX',
-    'height' : 'screenSizeY'
-  }),
+var layout = {};
 
-  pushTop: slate.operation('push', {
-    'direction' : 'top',
-    'style' : 'bar-resize:screenSizeY/2'
-  }),
+layout.full = S.op('move', {
+  x: 'screenOriginX',
+  y: 'screenOriginY',
+  width: 'screenSizeX',
+  height: 'screenSizeY'
+});
 
-  pushBottom: slate.operation('push', {
-    'direction' : 'bottom',
-    'style' : 'bar-resize:screenSizeY/2'
-  }),
+layout.topHalf = layout.full.dup({
+  height: 'screenSizeY/2'
+});
 
-  pushRight: S.op('push', {
-    'direction' : 'right',
-    'style' : 'bar-resize:screenSizeX/2'
-  }),
+layout.bottomHalf = layout.topHalf.dup({
+  y: 'screenOriginY+screenSizeY/2'
+});
 
-  pushLeft: S.op('push', {
-    'direction' : 'left',
-    'style' : 'bar-resize:screenSizeX/2'
-  })
-};
+layout.leftHalf = layout.full.dup({
+  width: 'screenSizeX/2'
+});
+
+layout.rightHalf = layout.leftHalf.dup({
+  x: 'screenOriginX+screenSizeX/2'
+});
+
+layout.topLeftQuadrant = layout.leftHalf.dup({
+  height: 'screenSizeY/2'
+});
+
+layout.bottomLeftQuadrant = layout.topLeftQuadrant.dup({
+  y: 'screenSizeY/2'
+});
+
+layout.topRightQuadrant = layout.rightHalf.dup({
+  height: 'screenSizeY/2'
+});
+
+layout.bottomRightQuadrant = layout.topRightQuadrant.dup({
+  y: 'screenSizeY/2'
+});
 
 var operations = {
-  'grid': S.op('grid'),
+  grid: S.op('grid', {
+    padding: '5'
+  }),
 
   throw0: S.op('throw', {
-    'screen': '0',
-    'width': 'screenSizeX',
-    'height': 'screenSizeY'
+    screen: '0',
+    width: 'screenSizeX',
+    height: 'screenSizeY'
   }),
 
   throw1: S.op('throw', {
-    'screen' : '1',
-    'width' : 'screenSizeX',
-    'height' : 'screenSizeY'
+    screen: '1',
+    width: 'screenSizeX',
+    height: 'screenSizeY'
   })
 };
 
 slate.bindAll({
   // Directionals
-  'up:ctrl;cmd;shift': layout.maximize,
-  'up:ctrl;cmd': layout.pushTop,
-  'down:ctrl;cmd': layout.pushBottom,
-  'right:ctrl;cmd': layout.pushRight,
-  'left:ctrl;cmd': layout.pushLeft,
+  'up:ctrl;cmd;shift': layout.full,
+  'up:ctrl;cmd': layout.topHalf,
+  'down:ctrl;cmd': layout.bottomHalf,
+  'left:ctrl;cmd': layout.leftHalf,
+  'right:ctrl;cmd': layout.rightHalf,
+  'j:ctrl;cmd': layout.topLeftQuadrant,
+  'k:ctrl;cmd': layout.bottomLeftQuadrant,
+  'l:ctrl;cmd': layout.topRightQuadrant,
+  ';:ctrl;cmd': layout.bottomRightQuadrant,
 
   // Monitors
   '[:ctrl;cmd': operations.throw0,
