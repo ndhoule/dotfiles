0bfa76ac0c5496c0ecddc5be8d82b28c9f2031f7
Nathan Houle <nathan@nathanhoule.com>
2014-05-14 16:00:22 -0700

    Add git aliases

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -8,20 +8,41 @@
 #
 
 # If hub is installed, use it as a Git wrapper
-# [[ -x $(which hub) ]] && alias git=hub
+[[ -x $(which hub) ]] && alias git=hub
 
 alias gW='git clean -f -d && git reset --hard'
 alias ga='git add'
 alias gst='git status'
 alias gc='git commit'
 alias gco='git checkout'
+alias gf='git fetch'
+alias gfo='git fetch origin'
+alias gfom='git fetch origin master'
+alias gfu='git fetch upstream'
+alias gfum='git fetch upstream master'
 alias gl='git pull'
+alias glo='git pull origin'
 alias glom='git pull origin master'
+alias glu='git pull upstream'
+alias glum='git pull upstream master'
 alias gp='git push'
+alias gpo='git push origin'
 alias gpom='git push origin master'
+alias gpu='git push upstream'
+alias gpum='git push upstream master'
 alias gd='git diff'
 alias gb='git branch'
 alias gba='git branch -a'
 alias del='git branch -d'
 alias gpr='git remote prune'
-alias gpr-local='git branch --merged | grep -v "^* master$" | grep -v "^  master$" | xargs git branch -d'
+# Prune remote/origin branches that have been merged. Skips master and the
+# currently checked-out branch
+alias gprl='git branch --merged | \
+            grep -v "^\(  \|* \)master$" | \
+            grep -v "^* $(git rev-parse --abbrev-ref HEAD)$" | \
+            xargs git branch -d'
+
+# Much doge, very alias
+alias such=git
+alias very=git
+alias wow='git status'
