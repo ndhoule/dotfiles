e8c124678c76f033d6cf969f34abc20d2fd43761
Nathan Houle <nathan@nathanhoule.com>
2014-09-15 10:47:52 -0700

    Alias http-serve to http-server whenever possible

--- zsh/functions.zsh
+++ zsh/functions.zsh
@@ -9,8 +9,16 @@
 
 # Launch a static server in the current directory, passing an optional port
 # number
-server() {
-  open "http://localhost:${1:-8000}" && $(which python) -m SimpleHTTPServer ${1:-8000}
+http-serve() {
+  # Prefer http-server (`npm install -g http-server`) when available; fall back
+  # to a simple Python SimpleHTTPServer when unavailable. Note that
+  # SimpleHTTPServer has performance problems when serving more than a few files
+  # simultaneously (e.g. an HTML file with more than 2-3 linked files)
+  if [[ -x $(which http-server) ]]; then
+    $(which http-server)
+  else
+    $(which python) -m SimpleHTTPServer
+  fi
 }
 
 # Keep a process running, restarting it if it crashes
