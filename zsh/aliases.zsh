efba9576c71159b0cc49e20b86065a6ec0436e15
Nathan Houle <nathan@nathanhoule.com>
2014-11-09 22:34:00 -0800

    More reorganization, minor edits

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -8,22 +8,24 @@
 #   Nathan Houle <nathan@nathanhoule.com>
 #
 
-alias l='ls -alh'
-alias ll='ls -lh'
-alias lll='ls -ah'
+alias l="ls -alh"
+alias ll="ls -lh"
+alias lll="ls -ah"
+
+alias gosrc="cd ${SRC_PATH}"
 
 # more is always less
 if [[ -x $(which less) ]]; then
-  alias more='less'
+  alias more="less"
 fi
 
 # Make sure stuff piped through less retains color
-alias less='less -R'
+alias less="less -R"
 
-# Shortcuts for fixing dumb repls that don't know about arrow keys
+# Shortcuts for fixing dumb repls that don"t know about arrow keys
 if [[ -x $(which rlwrap) ]]; then
-  alias clj='rlwrap clj'
-  alias clojure='rlwrap clj'
-  alias racket='rlwrap racket'
-  alias sml='rlwrap sml'
+  alias clj="rlwrap clj"
+  alias clojure="rlwrap clj"
+  alias racket="rlwrap racket"
+  alias sml="rlwrap sml"
 fi
