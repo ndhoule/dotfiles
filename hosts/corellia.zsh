0a55bcfb7c2cd579eaeb115be18a9cf4b763345a
Nathan Houle <nathan@nathanhoule.com>
2016-03-06 11:34:42 -0800

    Corellia: Update config

--- hosts/corellia.zsh
+++ hosts/corellia.zsh
@@ -5,14 +5,8 @@
 #   Nathan Houle <nathan@nathanhoule.com>
 #
 
-function {
-  local SEGMENT_DOTFILES_PATH="${DEV_PATH}/segmentio/dotfiles/source"
-  declare -a FILES
-  FILES=(general go npm release vm)
-
-  for file in "${FILES[@]}"; do
-    if [[ -f "${SEGMENT_DOTFILES_PATH}/${file}.sh" ]]; then
-      source "${SEGMENT_DOTFILES_PATH}/${file}.sh"
-    fi
-  done
+function vm() {
+  goto docker-vm
+  eval "$(make shellinit)"
+  make ssh
 }
