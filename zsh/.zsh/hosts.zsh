6f9ea12db43f8a49c18a3fece49fd64dbd762e3a
Nathan Houle <nathan@nathanhoule.com>
2018-01-09 09:41:14 -0800

    Add host-specific zsh config

---
+++ zsh/.zsh/hosts.zsh
@@ -0,0 +1,27 @@
+# vi:syntax=zsh
+# -*- mode: sh -*-
+
+#
+# Host-specific settings.
+#
+# TODO: This should be broken out into .env files too
+#
+# Author:
+#   Nathan Houle <nathan@nathanhoule.com>
+#
+
+if [[ "$(hostname)" == "endor.local" ]]; then
+    export GOTO_ORG="goodeggs"
+
+    if [ -f "${HOME}/.sekret" ]; then
+        source "${HOME}/.sekret"
+    fi
+
+    #if [ -f "${HOME}/.npmrc" ]; then
+    #    export NPM_AUTH=$(cat "${HOME}/.npmrc" | grep '^//npm.goodeggs.com/:_authToken=' | sed 's#//npm.goodeggs.com/:_authToken=##')
+    #fi
+
+    export PATH="$HOME/.yarn/bin:$PATH"
+    export PATH="${PATH}:${HOME}/.config/yarn/global/node_modules/.bin"
+    export PATH="./node_modules/.bin:${PATH}" # locally installed node binaries
+fi
