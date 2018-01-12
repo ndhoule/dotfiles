dadcd6821a84a3b8a5734f6e82881aef84eae28c
Nathan Houle <nathan@nathanhoule.com>
2018-01-11 21:43:23 -0800

    Update endor config

--- zsh/.zsh/hosts.zsh
+++ zsh/.zsh/hosts.zsh
@@ -10,16 +10,16 @@
 #   Nathan Houle <nathan@nathanhoule.com>
 #
 
-if [[ "$(hostname)" == "endor.local" ]]; then
+if [[ "$(hostname)" == "endor" ]]; then
     export GOTO_ORG="goodeggs"
 
     if [ -f "${HOME}/.sekret" ]; then
         source "${HOME}/.sekret"
     fi
 
-    #if [ -f "${HOME}/.npmrc" ]; then
-    #    export NPM_AUTH=$(cat "${HOME}/.npmrc" | grep '^//npm.goodeggs.com/:_authToken=' | sed 's#//npm.goodeggs.com/:_authToken=##')
-    #fi
+    if [ -f "${HOME}/.npmrc" ]; then
+       export NPM_AUTH=$(cat "${HOME}/.npmrc" | grep '^//npm.goodeggs.com/:_authToken=' | sed 's#//npm.goodeggs.com/:_authToken=##')
+    fi
 
     export PATH="$HOME/.yarn/bin:$PATH"
     export PATH="${PATH}:${HOME}/.config/yarn/global/node_modules/.bin"
