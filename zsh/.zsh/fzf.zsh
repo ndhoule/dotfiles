5f75014828196923b6c8253cbd05ad8a73e9c307
Nathan Houle <nathan@nathanhoule.com>
2018-02-25 23:11:46 -0800

    Source fzf zsh integration if available

---
+++ zsh/.zsh/fzf.zsh
@@ -0,0 +1,12 @@
+# -*- mode: sh -*-
+
+#
+# fzf (https://github.com/junegunn/fzf) configuration.
+#
+# Author:
+#   Nathan Houle <nathan@nathanhoule.com>
+#
+
+if [ -f "${HOME}/.fzf.zsh" ]; then
+  source "${HOME}/.fzf.zsh"
+fi
