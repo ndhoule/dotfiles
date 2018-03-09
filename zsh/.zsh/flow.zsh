8086b37f7579bef4047a1937017c76f9487f97fe
Nathan Houle <nathan@nathanhoule.com>
2018-03-09 00:09:19 -0800

    zsh: Improve flow server starter chpwd function

--- zsh/.zsh/flow.zsh
+++ zsh/.zsh/flow.zsh
@@ -10,9 +10,18 @@
 
 function flow_start_server() {
   emulate -L zsh
-  if [[ -f ".flowconfig" ]]; then
-    # TODO: Suppress output
-    yarn run flow server 2>&1 &
+
+  # Silence PID output when backgrounding task
+  setopt LOCAL_OPTIONS NO_NOTIFY NO_MONITOR
+
+  local NO_AUTOSTART_FLOW_SERVER=${NO_AUTOSTART_FLOW_SERVER:-}
+
+  if [[ -f ".flowconfig" && -x "./node_modules/.bin/flow" && -z "${NO_AUTOSTART_FLOW_SERVER}" ]]; then
+    echo "Starting a flow server."
+    # Run server in background and disown to mute notification
+    # nohup outputs when process exits
+    nohup ./node_modules/.bin/flow start &> /dev/null & disown
   fi
 }
+
 chpwd_functions=(${chpwd_functions[@]} "flow_start_server")
