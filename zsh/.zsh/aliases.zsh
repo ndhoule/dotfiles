44fafb9337628a10021ae7032c1b14fb03fa4622
Nathan Houle <nathan@nathanhoule.com>
2018-10-15 10:58:16 -0700

    Move git zsh aliases into .zsh subdirectory

--- zsh/.zsh/aliases.zsh
+++ zsh/.zsh/aliases.zsh
@@ -35,34 +35,6 @@ if [[ -x $(which hub) ]]; then
   eval "$(hub alias -s)"
 fi
 
-# Git aliases
-alias gW='git clean -f -d && git reset --hard'
-alias ga='git add'
-alias gb='git branch'
-alias gba='git branch -a'
-alias gc='git commit'
-alias gcl='git clone'
-alias gco='git checkout'
-alias gd='git diff'
-alias gf='git fetch'
-alias gfo='git fetch origin'
-alias gfom='git fetch origin master'
-alias gfu='git fetch upstream'
-alias gfum='git fetch upstream master'
-alias gl='git pull'
-alias glo='git pull origin'
-alias glom='git pull origin master'
-alias glu='git pull upstream'
-alias glum='git pull upstream master'
-alias gp='git push'
-alias gpo='git push origin'
-alias gpom='git push origin master'
-alias gpr='git remote prune'
-alias gprl='git-delete-merged'
-alias gpu='git push upstream'
-alias gpum='git push upstream master'
-alias gst='git status'
-alias gup='git-update'
 
 # Patch ag to use a global ignore list
 alias ag="ag --path-to-ignore=${HOME}/.agignore"
